344ccf4c8837e3c2a8885b804870a449ba3f7984
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2016-09-24 11:18:09 -0700

    created server

---
+++ server.js
@@ -0,0 +1,65 @@
+// Dependencies
+// =============================================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// Star Wars Characters (DATA)
+
+var reservations = [{
+    customerName: "Jonathan Arellano",
+    phoneNumber: "455-343-4344",
+    customerEmail: "NeverHacked@yahoo.com",
+    customerID: "44953"
+}, {
+    customerName: "Ramiro Lopez",
+    phoneNumber: "838-928-9383",
+    customerEmail: "r@lopez.com",
+    customerID: "asdfasf"
+}, {
+    customerName: "Tam",
+    phoneNumber: "938-282-8288",
+    customerEmail: "asdfas",
+    customerID: "1"
+}, {
+    customerName: "Ivette",
+    phoneNumber: "929-292-9229",
+    customerEmail: "ivette@gmail.com",
+    customerID: "4234"
+}];
+
+//Routes
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.get('/reserve', function(req, res) {
+    res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+
+app.get('/table', function(req, res) {
+    res.sendFile(path.join(__dirname, 'table.html'));
+});
+
+app.get('/api/waitlist', function(req, res) {
+    res.json(reservations);
+});
+
+app.get('/api/tables', function(req, res) {
+    res.json(reservations);
+});
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT: " + PORT);
+})
